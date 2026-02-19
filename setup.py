558dcdeecbcd1c458587458d7a49059c61eb9b28
RaccoNet <zavolokinyegor@gmail.com>
2026-02-19 11:00:23 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rapid-roman-nums",
+    version="1.0.0",
+    description="Rapid convert to/from roman numerals",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="raccoonette",
+    url="https://github.com/raccoonette/rapid-roman-nums",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
